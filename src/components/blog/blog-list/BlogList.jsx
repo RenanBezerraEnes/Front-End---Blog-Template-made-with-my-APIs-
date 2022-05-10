b1199bc63e0e203207f3bdca9865c4e9b22b6e3f
Renan Bezerra <renanbezerraenes@icloud.com>
2022-05-10 22:03:00 +0200

    Fetch to get the blogs and print it dynamic

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
-  return (
-    <Row>
-      {posts.map((post) => (
-        <Col
-          md={4}
-          style={{
-            marginBottom: 50,
-          }}
-        >
-          <BlogItem key={post.title} {...post} />
-        </Col>
-      ))}
-    </Row>
-  );
+	const [blogs, setBlogs] = useState([]);
+
+	const getBlogs = async () => {
+		const response = await fetch("http://localhost:3001/blogPosts", {
+			headers: { "Content-type": "application/json" },
+		});
+		const blogsApi = await response.json();
+		console.log(blogsApi);
+		if (response.ok) {
+			setBlogs(blogsApi.blog);
+		} else {
+			alert("Blogs Error");
+		}
+	};
+
+	useEffect(() => {
+		getBlogs();
+	}, []);
+
+	return (
+		<Row>
+			{blogs.map((blog) => (
+				<Col
+					md={4}
+					style={{
+						marginBottom: 50,
+					}}
+				>
+					<BlogItem key={blog._id} {...blog} />
+				</Col>
+			))}
+		</Row>
+	);
 };
 
 export default BlogList;
